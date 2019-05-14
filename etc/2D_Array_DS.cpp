ab39a3e212697df2e65ae069f58f54e7b0801b60
rnhappysmile <rnhappysmile@gmail.com>
2019-05-14 23:56:08 +0900

    feat: 2D_Array_DS.cpp 파일추가
    문제의 링크: https://www.hackerrank.com/challenges/2d-array/problem
    - 2D 배열 문제
    - 최적화 할 부분이 있음

--- etc/2D_Array_DS.cpp
+++ etc/2D_Array_DS.cpp
@@ -5,40 +5,58 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include <queue>
 
 using namespace std;
 
+#define MIN_VALUE -256
+
 bool Inside(int x, int y)
 {
-	return ((x >= 0) || (y >= 0) || (x < 6) || (y < 6));
+	return ((x >= 0) && (y >= 0) && (x < 6) && (y < 6));
 }
 
 // Complete the hourglassSum function below.
 int hourglassSum(vector<vector<int>> arr) {
 	vector<vector<int>> visited;	
 	int dir[8][2] = { {0, 0}, {0, 1}, {0, 2}, {1, 1}, {2, 0}, {2, 1}, {2, 2} };
-	int answer[36] = { 0, };
+	int answer = MIN_VALUE;
+	int temp_answer = 0;
+	int answer_flag = true;
 
 	for (int i = 0; i < 6; i++) {
-		int answer_count = 0;
 		for (int j = 0; j < 6; j++) {
 			for (int count = 0; count < 7; count++) {
-				if(Inside(i+dir[count][0], j+dir[count][1])){
-					answer[answer_count] += arr[i + dir[count][0]][j + dir[count][1]];
+				if((Inside(i+dir[count][0], j+dir[count][1])) &&
+					(arr[i + dir[count][0]][j + dir[count][1]] >= -9) &&
+					(arr[i + dir[count][0]][j + dir[count][1]] <= 9)){
+					temp_answer += arr[i + dir[count][0]][j + dir[count][1]];
 				}
 				else {
+					temp_answer = 0;
+					answer_flag = false;
 					break;
 				}
 			}
-			answer_count++;
+			if (answer_flag != false) {
+				if (answer < temp_answer){
+					answer = temp_answer;
+				}
+				temp_answer = 0;
+			}
+			answer_flag = true;
 		}
 	}
+
+	return answer;
 }
 
 int main()
 {
-	ofstream fout(getenv("OUTPUT_PATH"));
+	//ofstream fout(getenv("OUTPUT_PATH"));
+
+	cout << "hello" << endl;
 
 	vector<vector<int>> arr(6);
 	for (int i = 0; i < 6; i++) {
@@ -53,9 +71,9 @@ int main()
 
 	int result = hourglassSum(arr);
 
-	fout << result << "\n";
+	//fout << result << "\n";
 
-	fout.close();
+	//fout.close();
 
 	return 0;
 }
